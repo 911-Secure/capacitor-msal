b8769b66c2bf694efea47cbba0dbcf4ec9c56cdf
Ben Callaghan <bcallaghan@911secure.com>
2019-06-15 21:57:48 -0600

    Implemented login for Electron

--- electron/main/capacitor-msal.ts
+++ electron/main/capacitor-msal.ts
@@ -1,26 +1,8 @@
-import fs from 'fs';
-import os from 'os';
-import base64Url from 'base64url';
-import FormData from 'form-data';
-import jwtDecode from 'jwt-decode';
 import promiseIpc from 'electron-promise-ipc';
-import keytar from 'keytar';
-import { createHash, randomBytes } from 'crypto';
+import fetch from 'electron-fetch';
+import FormData from 'form-data';
 import { BrowserWindow } from 'electron';
-import { User, TokenResponse } from 'capacitor-msal';
-import { default as fetch, Response } from 'electron-fetch';
-import { Logger, NoOpLogger } from './logger';
-
-const keytarService = 'com.secure911.gatekeeper';
-const keytarAccount = os.userInfo().username;
-
-function random(bytes = 32): string {
-	return base64Url.encode(randomBytes(bytes));
-}
-
-function codeChallenge(codeVerifier: string): string {
-	return base64Url.encode(createHash('sha256').update(codeVerifier).digest());
-}
+import { LoginRequest, TokenResponse, ErrorResponse } from '../definitions';
 
 function buildFormData(obj: any): FormData {
 	return Object.keys(obj).reduce((data, key) => {
@@ -29,192 +11,73 @@ function buildFormData(obj: any): FormData {
 	}, new FormData());
 }
 
-interface MsalOptions {
-	window?: BrowserWindow;
-	logger?: Logger | null;
-	tenant?: string;
-	redirectUri?: string;
-	clientId?: string;
-	scopes?: string[];
-}
-
-export class AuthError extends Error {
-	constructor(errorResponse: any) {
-		super(errorResponse.error_description);
-		Object.assign(this, errorResponse);
-	}
-}
-
 export class CapacitorMsal {
-	private options: MsalOptions;
-	private logger: Logger;
-	private tokens: TokenResponse;
-	private tokensExpireAt: Date;
-
-	init(options?: MsalOptions) {
-		this.options = Object.assign({ logger: console }, options);
-
-		// Replace null loggers with the NoOpLogger.
-		this.logger = this.options.logger || new NoOpLogger();
-
-		// Read the config file, if it exists.
-		try {
-			const file = fs.readFileSync('./capacitor.config.json', 'utf-8');
-			const capConfig = JSON.parse(file).plugins.Msal;
-			Object.assign(this.options, capConfig);
-		} catch (error) {
-			this.logger.warn('MSAL - Unable to read MSAL configuration from capacitor.config.json', error);
-		}
-
-		this.logger.debug('MSAL - Registering IPC event handlers')
-		promiseIpc.on('capacitor-msal-login', () => this.login());
-		promiseIpc.on('capacitor-msal-acquire-token', () => this.acquireToken());
+	constructor() {
+		promiseIpc.on('msal-login', request => this.login(request));
 	}
 
-	private async login(): Promise<User> {
-		this.logger.info('MSAL - Logging in the user');
-		try {
-			// Attempt to log in silently.
-			await this.refreshTokens();
-		} catch (error) {
-			this.logger.warn('MSAL - Unable to refresh tokens. Attempting interactive login.', error);
-			await this.loginInteractive();
-		}
+	private async login(request: LoginRequest): Promise<TokenResponse> {
+		// Login using a popup.
+		const code = await new Promise<string>((resolve, reject) => {
+			let receivedResponse = false;
 
-		return jwtDecode<User>(this.tokens.id_token);
-	}
-
-	private async acquireToken(): Promise<TokenResponse> {
-		this.logger.info('MSAL - Acquiring access token');
-		await this.refreshTokens();
-		return this.tokens;
-	}
-
-	private async loginInteractive() {
-		// Generate the nonces used by OAuth.
-		const state = random();
-		const codeVerifier = random();
-		const challenge = codeChallenge(codeVerifier);
-
-		// Build the authorization URL.
-		const authorizeUrl = new URL(`https://login.microsoftonline.com/${this.options.tenant}/oauth2/v2.0/authorize`);
-		const authParams = new URLSearchParams({
-			client_id: this.options.clientId,
-			response_type: 'code',
-			redirect_uri: this.options.redirectUri,
-			scope: this.options.scopes.join(' '),
-			response_mode: 'query',
-			state: state,
-			code_challenge_method: 'S256',
-			code_challenge: challenge
-		});
-		authorizeUrl.search = authParams.toString();
-
-		// Open the user's browser to the sign-in page.
-		this.logger.debug('MSAL - Opening browser to the authorize endpoint');
-		const redirectUrl = await new Promise<URL>(resolve => {
 			const window = new BrowserWindow({
 				width: 1000,
 				height: 600,
-				show: false,
-				parent: this.options.window,
-				modal: !!this.options.window,
-				webPreferences: {
-					nodeIntegration: false
+				show: false
+				// TODO: Make this window a modal
+			});
+			window.loadURL(request.authorizeUrl);
+			window.on('ready-to-show', () => window.show());
+			window.on('closed', () => {
+				if (!receivedResponse) {
+					reject({ error: 'user_cancelled' });
 				}
 			});
 
-			window.loadURL(authorizeUrl.href);
-
-			window.on('ready-to-show', () => {
-				window.show();
-			});
-
-			window.webContents.on('will-redirect', (event, url) => {
-				event.preventDefault();
+			window.webContents.on('will-redirect', (_event, url) => {
+				receivedResponse = true;
 				window.close();
-				resolve(new URL(url));
+
+				const response = new URL(url).searchParams;
+				if (response.has('error')) {
+					reject({
+						error: response.get('error'),
+						error_description: response.get('error_description')
+					});
+				} else {
+					const responseState = response.get('state');
+					if (request.state === responseState) {
+						resolve(response.get('code'));
+					} else {
+						reject({ error: 'invalid_state', state: responseState });
+					}
+				}
 			});
 		});
 
-		if (redirectUrl.searchParams.has('error')) {
-			throw new AuthError({
-				error: redirectUrl.searchParams.get('error'),
-				error_description: redirectUrl.searchParams.get('error_description')
-			});
-		}
-
-		const responseState = redirectUrl.searchParams.get('state');
-		if (state !== responseState) {
-			throw new AuthError({
-				error: 'invalid_state',
-				error_description: 'The response state does not match the request state.'
-			});
-		}
-
-		// TODO: Add validation
-
-		// Exchange the authorization code for a set of tokens.
-		this.logger.debug('MSAL - Exchanging authorization code for user tokens');
-		const response = await fetch(`https://login.microsoftonline.com/${this.options.tenant}/oauth2/v2.0/token`, {
+		const response = await fetch(request.tokenUrl, {
 			method: 'POST',
 			body: buildFormData({
-				client_id: this.options.clientId,
+				client_id: request.clientId,
 				grant_type: 'authorization_code',
-				scope: this.options.scopes.join(' '),
-				code: redirectUrl.searchParams.get('code'),
-				redirect_uri: this.options.redirectUri,
-				code_verifier: codeVerifier
+				scope: request.scope,
+				code: code,
+				redirect_uri: request.redirectUri,
+				// TODO: code_verifier
 			})
 		});
-		await this.processTokenResponse(response);
+		const tokenResponse = await response.json<TokenResponse | ErrorResponse>();
+
+		if (this.isError(tokenResponse)) {
+			throw tokenResponse;
+		}
+
+		// TODO: Save refresh token
+		return tokenResponse;
 	}
 
-	private async refreshTokens() {
-		// No action needed if the tokens have not expired.
-		if (this.tokensExpireAt > new Date()) {
-			this.logger.debug('MSAL - Existing tokens are still valid');
-			return;
-		}
-
-		this.logger.debug('MSAL - Retrieving most recent refresh token');
-		const refreshToken = await keytar.getPassword(keytarService, keytarAccount);
-		if (!refreshToken) {
-			throw new AuthError({
-				error: 'interaction_required',
-				error_description: 'There is no refresh token available.'
-			});
-		}
-
-		this.logger.info('MSAL - Refreshing user access tokens');
-		const response = await fetch(`https://login.microsoftonline.com/${this.options.tenant}/oauth2/v2.0/token`, {
-			method: 'POST',
-			body: buildFormData({
-				client_id: this.options.clientId,
-				grant_type: 'refresh_token',
-				scope: this.options.scopes.join(' '),
-				refresh_token: refreshToken
-			})
-		});
-		await this.processTokenResponse(response);
-	}
-
-	private async processTokenResponse(response: Response) {
-		const tokens = await response.json();
-		if (tokens.error) {
-			throw new AuthError(tokens);
-		}
-
-		// Save the received tokens.
-		this.tokens = tokens;
-
-		// Compute when the tokens will expire.
-		this.tokensExpireAt = new Date(Date.now() + (1000 * this.tokens.expires_in));
-
-		// Store the refresh token to login silently.
-		this.logger.debug('MSAL - Saving refresh token');
-		keytar.setPassword(keytarService, keytarAccount, this.tokens.refresh_token);
+	private isError(response: TokenResponse | ErrorResponse): response is ErrorResponse {
+		return (<ErrorResponse>response).error !== undefined;
 	}
 }
-
-export default new CapacitorMsal();
--- electron/renderer/index.ts
+++ electron/renderer/index.ts
@@ -1,10 +1,15 @@
 import promiseIpc from 'electron-promise-ipc';
+import uuid from 'uuid/v4';
 import { WebPlugin } from '@capacitor/core';
-import { MsalPlugin, User, TokenResponse } from 'capacitor-msal';
+import { Configuration, AuthenticationParameters, AuthResponse, ServerError, ClientAuthError, Account, InteractionRequiredAuthError } from 'msal';
+import { ClientInfo } from 'msal/lib-commonjs/ClientInfo';
+import { IdToken } from 'msal/lib-commonjs/IdToken';
+import { MsalPlugin } from 'capacitor-msal';
+import { LoginRequest, ErrorResponse, TokenResponse } from '../definitions';
 
 export class MsalElectron extends WebPlugin implements MsalPlugin {
-	private _user: User;
-	
+	private options: Configuration;
+
 	constructor() {
 		super({
 			name: 'Msal',
@@ -12,16 +17,114 @@ export class MsalElectron extends WebPlugin implements MsalPlugin {
 		});
 	}
 
-	get user(): User {
-		return this._user;
+	init(options: Configuration): Promise<void> {
+		// The following defaults come from the MSDN documentation.
+		// https://docs.microsoft.com/en-us/azure/active-directory/develop/msal-js-initializing-client-applications
+		this.options = Object.assign<Configuration, Configuration>({
+			auth: {
+				clientId: undefined, // This must be set by the user.
+				authority: 'https://login.microsoftonline.com/common',
+				validateAuthority: true, // TODO: Validate issuer
+				redirectUri: window.location.href,
+				navigateToLoginRequestUrl: true
+			},
+			cache: {
+				cacheLocation: 'sessionStorage', // TODO: Add caching
+				storeAuthStateInCookie: false // TODO: Use cookies
+			},
+			system: {
+				// TODO: logger
+				loadFrameTimeout: 6000,
+				tokenRenewalOffsetSeconds: 300
+			}
+		}, options);
+
+		// The default value of postLogoutRedirectUri is the final value of redirectUri.
+		if (!this.options.auth.postLogoutRedirectUri)
+			this.options.auth.postLogoutRedirectUri = this.options.auth.redirectUri;
+
+		return Promise.resolve();
 	}
 
-	login(): Promise<User> {
-		return promiseIpc.send('capacitor-msal-login');
+	async login(request?: AuthenticationParameters): Promise<AuthResponse> {
+		// Normalize and set default values.
+		request = Object.assign<AuthenticationParameters, AuthenticationParameters>({
+			scopes: [],
+			extraScopesToConsent: [],
+			authority: this.options.auth.authority
+		}, request);
+
+		const redirectUri = this.options.auth.redirectUri instanceof Function
+			? this.options.auth.redirectUri()
+			: this.options.auth.redirectUri;
+		const state = `${uuid()}|${request.state}`;
+		const scope = [...request.scopes, ...request.extraScopesToConsent].join(' ');
+
+		// Build the authorize URL.
+		const url = new URL(`${request.authority}/oauth2/v2.0/authorize`);
+		url.search = new URLSearchParams({
+			client_id: this.options.auth.clientId,
+			response_type: 'code',
+			redirect_uri: redirectUri,
+			scope: scope,
+			response_mode: 'query',
+			state: state,
+			prompt: request.prompt,
+			login_hint: request.loginHint,
+			// TODO: domain_hint
+			// code_challenge_method: 'S256',
+			// TODO: code_challenge
+			...request.extraQueryParameters
+		}).toString();
+
+		// The Electron main process will be used to control the popup window,
+		// capture its redirect, and exchange the code for a token.
+		// The Window APIs are cleaner there, and Azure AD does not support CORS.
+		// Responses and errors are handled in the renderer to preserve class info.
+		try {
+			const response: TokenResponse = await promiseIpc.send('msal-login', {
+				authorizeUrl: url.href,
+				tokenUrl: `${request.authority}/oauth2/v2.0/token`,
+				state: state,
+				clientId: this.options.auth.clientId,
+				scope: scope,
+				redirectUri: redirectUri
+			} as LoginRequest);
+
+			const idToken = new IdToken(response.id_token);
+			return {
+				uniqueId: idToken.objectId || idToken.subject,
+				tenantId: idToken.tenantId,
+				tokenType: response.token_type,
+				idToken: idToken,
+				accessToken: response.access_token,
+				scopes: response.scope.split(' '),
+				expiresOn: new Date(Date.now() + response.expires_in * 1000),
+				account: Account.createAccount(idToken, new ClientInfo(response.client_info)),
+				accountState: request.state
+			};	
+		} catch (e) {
+			const response: ErrorResponse = e;
+			switch (response.error) {
+				case 'user_cancelled':
+					throw ClientAuthError.createUserCancelledError();
+				case 'invalid_state':
+					throw ClientAuthError.createInvalidStateError(response.state, state);
+				case 'interaction_required':
+				case 'login_required':
+					throw new InteractionRequiredAuthError(response.error, response.error_description);
+				default:
+					throw new ServerError(response.error, response.error_description);
+			}
+		}
 	}
 
-	acquireToken(): Promise<TokenResponse> {
-		return promiseIpc.send('capacitor-msal-acquire-token');
+	acquireTokenSilent(request: AuthenticationParameters): Promise<AuthResponse> {
+		throw new Error("Method not implemented.");
+	}
+
+	acquireTokenInteractive(request: AuthenticationParameters): Promise<AuthResponse> {
+		throw new Error("Method not implemented.");
 	}
 }
 
